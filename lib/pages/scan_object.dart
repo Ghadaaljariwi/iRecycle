6c0c1dc68a0a2a2849e6650cacfa7309f049fb47
Hay8 <hayaar2@gmail.com>
2023-01-16 21:25:49 +0300

    object recognition api

---
+++ lib/pages/scan_object.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  final imagePicker = ImagePicker();
+  final apiKey =
+      'https://console.cloud.google.com/iam-admin/serviceaccounts/details/110654529031264593730;edit=true?previousPage=%2Fapis%2Fapi%2Fvision.googleapis.com%2Fcredentials%3Fproject%3Dsacred-analyst-373515&project=sacred-analyst-373515';
+  String result = '';
+
+  Future<void> _getPermission() async {
+    var status = await Permission.camera.status;
+    if (status.isDenied) {
+      status = await Permission.camera.request();
+    }
+    if (status.isGranted) {
+      status = await Permission.photos.status;
+      if (status.isDenied) {
+        status = await Permission.photos.request();
+      }
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await imagePicker.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _sendImage(pickedFile.path);
+    }
+  }
+
+  Future<void> _sendImage(String path) async {
+    final response = await http.post(
+      Uri.parse('https://vision.googleapis.com/v1/images:annotate?key=$apiKey'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'requests': [
+          {
+            'image': {
+              'content': base64Encode(File(path).readAsBytesSync()),
+            },
+            'features': [
+              {
+                'type': 'OBJECT_LOCALIZATION',
+                'maxResults': 1,
+              },
+            ],
+          },
+        ],
+      }),
+    );
+    final decodedResponse = jsonDecode(response.body);
+    result = decodedResponse['responses'][0]['localizedObjectAnnotations'][0]
+        ['name'];
+    _showResult();
+  }
+
+  Future<void> _showResult() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Result'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(result),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 100,
+          color: Colors.yellow,
+          child: ElevatedButton(
+            child: Text('Take Picture'),
+            onPressed: () {
+              _getPermission();
+              _pickImage();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
