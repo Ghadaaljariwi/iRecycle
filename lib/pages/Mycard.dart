09986f0d2566e253a54b0b5927534ce934c35faf
najdqm <100093366+najdqm@users.noreply.github.com>
2023-01-17 23:22:21 +0300

    RecyclingTheme

--- lib/pages/Mycard.dart
+++ lib/pages/Mycard.dart
@@ -6,14 +6,24 @@ class MyCard extends StatelessWidget {
   final String category;
   final color;
   final img;
-  const MyCard({super.key, required this.category, this.color, this.img});
+  final double width;
+  final double h;
+  final double w;
+  const MyCard(
+      {super.key,
+      required this.category,
+      this.color,
+      this.img,
+      required this.width,
+      required this.h,
+      required this.w});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Container(
-        width: 150,
+        width: width,
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: color,
@@ -27,8 +37,8 @@ class MyCard extends StatelessWidget {
                 image: AssetImage(
                   img,
                 ),
-                height: 100,
-                width: 100,
+                height: h,
+                width: w,
               ),
             ),
             Center(
