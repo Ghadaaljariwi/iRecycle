e6e29d103d5b94ab116748d38d0597ee0478e57e
Hay8 <hayaar2@gmail.com>
2023-02-10 10:27:03 +0300

    3 eco friendly added and overflow solved

--- lib/pages/Mycard.dart
+++ lib/pages/Mycard.dart
@@ -24,75 +24,78 @@ class MyCard extends StatelessWidget {
     required this.w,
     required this.f,
     required this.link,
-    this.showLocationIcon = true,
+    this.showLocationIcon = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Container(
-        width: width,
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image(
-                image: AssetImage(
-                  img,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          width: width,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image(
+                  image: AssetImage(
+                    img,
+                  ),
+                  height: h,
+                  width: w,
                 ),
-                height: h,
-                width: w,
               ),
-            ),
-            Center(
-              child: Text(
-                category.toString(),
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: f),
+              Center(
+                child: Text(
+                  category.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: f),
+                ),
               ),
-            ),
-            if (showLocationIcon)
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 20, bottom: 20),
-                  child: InkWell(
-                    splashColor: Colors.grey,
-                    highlightColor: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(50),
-                    onTap: () async {
-                      if (await canLaunchUrl(Uri.parse(link))) {
-                        await launchUrl(Uri.parse(link));
-                      } else {
-                        throw 'Could not launch $link';
-                      }
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.grey,
-                      ),
-                      child: Icon(
-                        Icons.location_on,
-                        color: Colors.white,
+              if (showLocationIcon)
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 20, bottom: 20),
+                    child: InkWell(
+                      splashColor: Colors.grey,
+                      highlightColor: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: () async {
+                        if (await canLaunchUrl(Uri.parse(link))) {
+                          await launchUrl(Uri.parse(link));
+                        } else {
+                          throw 'Could not launch $link';
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey,
+                        ),
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
 
 // class MyCard extends StatelessWidget {
 //   final String category;
