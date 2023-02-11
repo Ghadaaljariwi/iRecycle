6240c95a77368d6f288cc5418b916b40d1982590
Raghad <rk.2002.kr@gmail.com>
2023-02-11 10:51:27 +0300

    class diagram

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:dcdg/dcdg.dart';
 
 Map<int, Color> color = {
   50: Color.fromARGB(255, 65, 102, 39),
